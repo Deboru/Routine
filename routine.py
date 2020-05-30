14ee1c7b1dff0edf40f1d14e8a36d7bb52c63099
Deboru <deblin0118@gmail.com>
2020-05-30 11:59:56 -0700

    update to loop

--- routine.py
+++ routine.py
@@ -6,7 +6,6 @@ print('And when do you plan to sleep?')
 sleeptime = input()
 
 routine = []
-textFile = open('C:\\Users\\debby\\Desktop\\routine\\Routine.txt', 'w')
 
 # TODO: Prompt user for the routine to do at each hour
 for i in range(int(starttime), int(sleeptime)):
@@ -14,10 +13,24 @@ for i in range(int(starttime), int(sleeptime)):
     routine.append(input())
 
 # TODO: Create new txt file with routine formatted on C:\Users\debby\Desktop\Routine
-print('Here is your schedule for today:')
-for i in range(int(starttime), int(sleeptime)):
-    print('%d:00 to %d:00 - %s' %(i, i+1, routine[i-int(starttime)]))
+while True:
+    print('Here is your schedule for today:')
+    for i in range(int(starttime), int(sleeptime)):
+        print('%d:00 to %d:00 - %s' %(i, i+1, routine[i-int(starttime)]))
+
+    print('Do you want to make any changes?')
+    yesno = input()
+    if (yesno.lower() == "y") or (yesno.lower() == "yes"):
+        print('What hour do you wish to change?')
+        changehour = int(input())
+        print('What do you plan to do from %d:00 to %d:00? : ' %(changehour, changehour+1))
+        routine[changehour - int(starttime)] = input()
+    else:
+        break
+
+print('Do you want to export to a text file?')
+exportyesno = input()
+if exportyesno.lower() == 'y' or 'yes':
+    textFile = open('C:\\Users\\debby\\Desktop\\routine\\Routine.txt', 'w')
     textFile.write('%d:00 to %d:00 - %s\n' %(i, i+1, routine[i-int(starttime)]))
-
-
-textFile.close()
+    textFile.close()
