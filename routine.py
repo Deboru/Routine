4a65984aa014f2e8ab8f1ec4cb32cbd36ad07106
Deboru <deblin0118@gmail.com>
2020-11-05 12:31:04 -0800

    New update - custom time steps

--- routine.py
+++ routine.py
@@ -1,6 +1,9 @@
 import easygui
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(message)s')
+logging.disable(logging.DEBUG) #uncomment for final 
 
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 
@@ -8,9 +11,9 @@ Tk().withdraw() # we don't want a full GUI, so keep the root window from appeari
 
 print('Good morning Debby')
 while True:
-    print('When did you wake up today?')
+    print('When did you wake up today? (please enter 24 hr clock)')
     starttime = input()
-    print('And when do you plan to sleep?')
+    print('And when do you plan to sleep? (please enter 24 hr clock)')
     sleeptime = input()
     if (starttime.isdigit() and sleeptime.isdigit()):
         break
@@ -18,38 +21,75 @@ while True:
         print('I\'m sorry, I\'m going to need you to input numbers\n')
 
 routine = []
+steps = []
+i = int(starttime)
 
 # TODO: Prompt user for the routine to do at each hour
-for i in range(int(starttime), int(sleeptime)):
-    print('What do you plan to do from %d:00 to %d:00? : ' %(i, i+1))
+while i < int(sleeptime):
+    print('What do you plan to do from %d:00? : ' %(i))
     routine.append(input())
+    if i+1 != int(sleeptime):
+        print('when will you finish? : ')
+        steps.append(int(input())-i)
+    else:
+        steps.append(1)
+    print('you plan to',routine[-1],'from %d:00 to %d:00' %(i,i+steps[-1]))
+    i += steps[-1]
+    logging.debug(i)
 
 # TODO: Create new txt file with routine formatted on C:\Users\debby\Desktop\Routine
 while True:
     print('Here is your schedule for today:')
-    for i in range(int(starttime), int(sleeptime)):
-        print('%d:00 to %d:00 - %s' %(i, i+1, routine[i-int(starttime)]))
+    k = 0
+    i = int(starttime)
+    while i < int(sleeptime):
+        print('%d:00 to %d:00 - %s' %(i, i+steps[k], routine[k]))
+        i += steps[k]
+        k += 1
 
     print('Do you want to make any changes?')
-    yesno = input()
+    yesno = str(input()).strip()
     if (yesno.lower() == "y") or (yesno.lower() == "yes"):
-        print('What hour do you wish to change?')
+        print('What start hour activity do you wish to change?')
         changehour = int(input())
-        print('What do you plan to do from %d:00 to %d:00? : ' %(changehour, changehour+1))
-        routine[changehour - int(starttime)] = input()
+        #TODO: What to do when the change hour is not in our range
+        print('What do you plan to do from %d:00 : ' %(changehour))
+
+        #find the corresponding routine to change
+        k = 0
+        while k < len(routine):
+            i = int(starttime)
+            while i < int(sleeptime):
+                if i == changehour:
+                    break
+                else:
+                    i += steps[k]
+                    k +=1
+            break
+        routine[k] = str(input())
+        
     else:
         break
 
 print('Do you want to export to a text file?')
-exportyesno = input()
-if exportyesno.lower() == 'y' or 'yes':
+exportyesno = str(input())
+if (exportyesno.lower() == 'y') or (exportyesno.lower() == 'yes'):
     print('Where do you want to save it to?')
     path = askdirectory() # show an "Open" dialog box and return the path to the selected file
     #path = easygui.diropenbox()
     print('What do you want to name it?')
     name = input()
     textFile = open(path+'\\'+name +'.txt', 'w')
-    for i in range(int(starttime), int(sleeptime)):
-        textFile.write('%d:00 to %d:00 - %s\n' %(i, i+1, routine[i-int(starttime)]))
+
+    k = 0
+    i = int(starttime)
+    while i < int(sleeptime):
+        textFile.write('%d:00 to %d:00 - %s\n' %(i, i+steps[k], routine[k]))
+        i += steps[k]
+        k += 1
+        
     print('The file ' + name+ '.txt has been saved in the directory ' + path)
     textFile.close()
+
+else:
+    print('Have a nice day!')
